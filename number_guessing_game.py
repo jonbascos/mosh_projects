bbd769d8eaad39ba4cfd05b50f0f881863e16f0a
Jon Bascos <38457147+jonbascos@users.noreply.github.com>
2024-11-28 00:42:11 -0800

    Check to make sure that the input is a number

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,12 +3,17 @@ import random
 answer = random.randint(1, 100)
 
 while True:
-    guess = int(input("Guess the number between 1 and 100: "))
-
-    if guess > answer:
-        print("Too high!")
-    elif guess < answer:
-        print("Too low!")
-    else:
-        print("Congratulations!  You guessed the number.")
-        break
+    try:
+        guess = int(input("Guess the number between 1 and 100: "))
+        
+        if guess > answer:
+            print("Too high!")
+        elif guess < answer:
+            print("Too low!")
+        else:
+            print("Congratulations!  You guessed the number.")
+            break
+    except ValueError:
+        print("Please enter a valid number: ")
+        
+    
